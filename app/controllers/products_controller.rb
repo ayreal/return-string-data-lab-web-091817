d63472249a784ed5538fdb6a2a5d8e1ed4b34bc7
Ariel Herman <aeh.herman@gmail.com>
2017-11-11 19:55:38 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,36 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render plain: @product.description
+  end
+
+  def inventory
+    product = Product.find_by(id: params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
+
+  def description
+    description = Product.find_by(id: params[:id]).description
+    render plain: description
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :inventory)
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@ products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "la
 
 10.times do
   products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
+  descriptions = ["jskh fsjkdhf skdfh sh jshdf sfs f", "rjehtfi uh rfisudhf ksjhdf skdhf", "rjth ksudfhiudf suhfi suhdfiuhsf uishfdiu "]
+  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500), :inventory => Faker::Number.between(0,120), :description => descriptions.sample)
 end
 5.times do
   Customer.create(:name => Faker::Name.name)
@@ -22,4 +23,3 @@ end
   order = Order.create(:customer_id => Customer.all.collect(&:id).sample, :invoice_id => Invoice.all.collect(&:id).sample)
   order.products << Product.find(Product.all.collect(&:id).sample)
 end
-
